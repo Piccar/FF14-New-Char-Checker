b2b35411272f4971d3c0387084f36ca22fd8ccdc
Fred <39131992+Piccar@users.noreply.github.com>
2022-07-30 01:35:59 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,19 @@
+import requests 
+from bs4 import BeautifulSoup
+import time
+import sys
+
+URL = 'https://eu.finalfantasyxiv.com/lodestone/worldstatus/'
+    
+while(True):
+    website = requests.get(URL)
+    results = BeautifulSoup(website.content, 'html.parser')
+    server = results.find_all('li', class_= 'item-list')
+    for server in server:
+        if server.find('p', string='Omega'):
+            if server.find('i', class_='world-ic__available js__tooltip'):
+                print("Server Free")
+                sys.exit(0)
+            if server.find('i', class_='world-ic__unavailable js__tooltip'):
+                print("Server Full")
+    time.sleep(5)
